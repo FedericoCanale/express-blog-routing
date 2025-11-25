62296317838a59dab8d0899ec3f7bec2436c7afd
FedericoCanale <icocanale@gmail.com>
2025-11-25 11:52:19 +0100

    Add posts router file

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
-
 app.use(express.json());
+
+
 app.use(express.static("public"));
+
+
+const postsRouter = require("./routers/posts");
+
+
+app.use("/posts", postsRouter);
+
 app.get("/", (req, res) => {
     res.send("Homepage del blog");
 });
